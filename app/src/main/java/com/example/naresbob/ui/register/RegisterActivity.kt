ec67b68709d505fb7c29f2410f76e7ffec4753a3
Galen Alvian <136723921+alviangalen@users.noreply.github.com>
2025-11-26 13:23:31 +0700

    Latihan 1

---
+++ app/src/main/java/com/example/naresbob/ui/register/RegisterActivity.kt
@@ -0,0 +1,50 @@
+package com.example.naresbob.ui.register
+
+import android.os.Build
+import android.os.Bundle
+import android.view.View
+import android.view.WindowInsetsController
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.naresbob.R
+import com.example.naresbob.databinding.ActivityLoginBinding
+import com.example.naresbob.databinding.ActivityRegisterBinding
+
+class RegisterActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityRegisterBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        enableEdgeToEdge()
+        setContentView(binding.root)
+
+        @Suppress("DEPRECATION")
+        window.statusBarColor = getColor(R.color.coklat_utama)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.setSystemBarsAppearance(
+                0, WindowInsetsController.APPEARANCE_LIGHT_STATUS_BARS
+            )
+        }
+
+        else {
+            @Suppress("DEPRECATION")
+            window.decorView.systemUiVisibility =
+                window.decorView.systemUiVisibility and View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR.inv()
+        }
+
+        ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        binding.tvSignin.setOnClickListener {
+            finish()
+        }
+    }
+}
